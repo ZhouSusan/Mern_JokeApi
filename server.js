a9bcef1602a3716ac216664cbf9558a66ce87b42
Susan <szhou089@gmail.com>
2022-03-19 22:17:41 -0700

    "update"

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const PORT = 8000;
     
 app.use(express.json(), express.urlencoded({ extended: true }));
 
-require("../server/config/mongoose.config");
+require("./server/config/mongoose.config");
     
-const AllMyJokeRoutes = require("../server/routes/joke.routes");
+const AllMyJokeRoutes = require("./server/routes/joke.routes");
 AllMyJokeRoutes(app);
     
 app.listen(PORT, () => console.log(`The server is all fired up on port ${PORT}`));
